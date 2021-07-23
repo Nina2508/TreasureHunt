f59d431258f20ef87ce693f496b49447a57d4a81
Nina2508 <75534456+Nina2508@users.noreply.github.com>
2021-07-23 19:57:52 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "WASHINGTON";
+const accessCode2 = "BEACH";
+const accessCode3 = "NEWYORK";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("S N T W I H N A G O", 100,50)
+    fill("lightblue")
+    text("Hint: Capitol of USA", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("C E A H B", 720,50)
+fill("lightblue")
+text("Hint: Place that has sand and water", 720,70)
+
+fill("white")
+textSize(15)
+text("K E O N Y W R", 500,400)
+fill("lightblue")
+text("Hint: Most popular city of the USA", 500,420)
+}
